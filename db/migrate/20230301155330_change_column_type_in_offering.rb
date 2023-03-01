0e156fa34c7438f17b8275c15209ca92ecae5915
vilma catani <vilma.catani@gmail.com>
2023-03-01 17:43:01 +0100

    revert data type

--- db/migrate/20230301155330_change_column_type_in_offering.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnTypeInOffering < ActiveRecord::Migration[7.0]
-  def change
-    change_column :bookings, :start_date, :string
-    change_column :bookings, :end_date, :string
-  end
-end
