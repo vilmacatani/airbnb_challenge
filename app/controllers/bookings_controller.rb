0797fcab255acf24eee0a6e95319a80ad99d2db5
vilma catani <vilma.catani@gmail.com>
2023-03-01 22:46:25 +0100

    create new booking works

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,43 @@
 class BookingsController < ApplicationController
+  before_action :set_offering, only: %i[new create]
+
   def index
     @my_bookings = Booking.where(user_id: current_user)
     @my_offerings = Offering.where(user_id: current_user)
     @my_offering_bookings = Booking.where(offering_id: @my_offerings)
   end
 
-  def new; end
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.pending = true
+    @booking.accepted = false
+    @booking.user = current_user
+    @booking.offering = @offering
+
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def show; end
 
   def destroy; end
 
-  def create; end
-
   def edit; end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def set_offering
+    @offering = Offering.find(params[:offering_id])
+  end
 end
