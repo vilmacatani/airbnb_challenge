17574bf0e5b5295bad743cb480517ebe84f22197
Marta Solenne <martasolenne.sol@gmail.com>
2023-03-03 14:11:28 +0100

    maps are good to go

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
     container: this.element,
     style: 'mapbox://styles/mapbox/dark-v11', // style URL
     center: [0, 0],
-    zoom: 0.8, // starting zoom
+    zoom: 4, // starting zoom
   });
 
   this.#addMarkersToMap()
@@ -39,6 +39,6 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 5, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 9, duration: 0 })
   }
 }
